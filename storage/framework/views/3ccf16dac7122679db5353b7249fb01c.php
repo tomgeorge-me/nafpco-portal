74da71bfe3a69bfdd0ff7dbcc14c73017a580681
Tom George <7488634+tomgeorge-me@users.noreply.github.com>
2026-08-06 10:07:52 +0530

    Resync repository after updating .gitignore

--- storage/framework/views/3ccf16dac7122679db5353b7249fb01c.php
+++
@@ -1,9 +0,0 @@
-
-<div class="seal" style="<?php echo e($style ?? ''); ?>">
-    <div class="seal-text">
-        <strong>Registered</strong>
-        Farmer Producer Co.
-        <br>Est. 2016 &middot; Kerala
-    </div>
-</div>
-<?php /**PATH /home/tom/webapps/laravel/nafpco-portal/resources/views/partials/seal.blade.php ENDPATH**/ ?>
